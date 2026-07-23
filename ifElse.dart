c978f29941ef62e432fd653ec9f6ace325a9b1fc
dev-nirob1 <dev.nirob1@gmail.com>
2026-07-23 17:05:35 +0600

    if else condition and switch case statement

---
+++ ifElse.dart
@@ -0,0 +1,16 @@
+void main(){
+  var marks = 33;
+  if(marks >=80){
+    print('Result is A+');
+  }
+  else if(marks >=70 && marks <=79){
+    print('Result is A');
+  }
+  else if(marks >=60 && marks <=69){
+    print('Result is A-');
+  }
+  else if(marks >=50 && marks <= 59 ){
+    print('Result is B');
+  }
+ else{ print('You passed the exam');}
+}
---
+++ swithCase.dart
@@ -0,0 +1,18 @@
+void main() {
+  var marks = 81;
+  switch (marks) {
+    case 80:
+      print('Result is A+');
+      break;
+    case 70:
+      print('Result is A-');
+      break;
+    case 60:
+      print('Result is B');
+      break;
+    default:
+      print('Result not found');
+      break;
+  }
+  print('switch case');
+}
